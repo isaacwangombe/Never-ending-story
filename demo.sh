1a9b067dc964505f266d0a703d76aa4cd9a0bd8f
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-01-06 08:45:48 +0300

    Adding conent

--- demo.sh
+++ demo.sh
@@ -85,8 +85,7 @@ git add-commit -m "Adding conent"
 echo "Hello 4 $USER" >> index.html
 echo "Today is $(date)" >> index.html
 
-git add .
-git commit --date “Wed Jan 4 14:00 2023 +0100”
+git add-commit -m "Adding conent"
 
 
 
